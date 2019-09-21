f857edefdba39256373dfe09f544aa8e7730fc6f
NATALIA MARIA HENAO CADAVID <nataliahenao40233@correo.itm.edu.co>
2019-09-21 17:43:49 -0500

    Assiteant commint

--- JobPortal.Web/Data/Entities/Program.cs
+++ JobPortal.Web/Data/Entities/Program.cs
@@ -12,7 +12,7 @@ namespace JobPortal.Web.Data.Entities
 
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Display(Name = "Program")]
+        [Display(Name = "Program")] 
         public string Name { get; set; } 
 
         [Required(ErrorMessage = "The field {0} is mandatory.")]
